755aee8ecf826d86607e67e1eea1d1dd1d06e6e5
M Bilal <mbilal75362@gmail.com>
2025-09-10 04:01:34 +0500

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,23 +22,23 @@ export default function Portfolio() {
       <ServicesSection />
       <TestimonialsSection />
       <ContactSection />
-      
+
       {/* Footer */}
       <footer className="bg-secondary py-5 border-t border-border">
         <div className="container mx-auto px-4">
-          <div className="flex flex-col md:flex-row md:items-center md:justify-between space-y-6 md:space-y-0">
-            {/* Logo */}
-            <div className="flex items-center">
-              <Image 
-                src="/Inzi_Sandhu.svg" 
-                alt="Inzamam Sandhu Logo" 
+          <div className="flex items-center justify-center relative">
+            {/* Logo Left */}
+            <div className="absolute left-0">
+              <Image
+                src="/Inzi_Sandhu.svg"
+                alt="Inzamam Sandhu Logo"
                 width={120}
                 height={48}
                 className="h-12 w-auto"
               />
             </div>
-            
-            {/* Copyright */}
+
+            {/* Copyright Center */}
             <p className="text-muted-foreground text-center">
               © 2025 Inzamam Sandhu. All rights reserved.
             </p>
